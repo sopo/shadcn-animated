67facace4c1a276f165d8b2f9ddeaad2fd6b774f
sopo <sofiko.imnaishvili@gmail.com>
2026-08-20 21:47:22 +0400

    update shell

--- src/components/domain/shell.tsx
+++ src/components/domain/shell.tsx
@@ -7,19 +7,13 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import {themes} from "prism-react-renderer"
-
-import { CodeBlock } from 'react-code-block';
-
-
+} from "@/components/ui/sheet";
 import type { PropsWithChildren } from "react";
-
 import { Copy, Plus } from "lucide-react";
 import { Button } from "../ui/button";
+import { Link } from "react-router";
 
 const Shell: React.FC<PropsWithChildren> = ({ children }) => {
-
   return (
     <div className="relative h-100 bg-card rounded-3xl flex items-center justify-center flex-col">
       <div>
@@ -44,69 +38,47 @@ const Shell: React.FC<PropsWithChildren> = ({ children }) => {
         >
           <Plus strokeWidth={2.5} />
         </Button>
-            <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
-  <SheetContent side="bottom" className="py-12 ">
-    <div className="max-w-3xl w-3xl mx-auto flex flex-col gap-4">
-       <SheetTitle className="text-xl font-medium">Button</SheetTitle>
-      <div>
-        <h2 className="text-lg">Install</h2>
-        <div className="">
-            <Bash code={`npm install wiii`} />
-    </div>
-      </div>
-    <div className=" items-center gap-4 bg-red-200 flex">
-     
-      <div className="flex-1 h-100 bg-card rounded-3xl flex items-center justify-center">
-        <Button>Click</Button>
-     </div>
-     <div className="flex-1">
-   
-      <Button className="outline">View Docs</Button>
-      <Button className="outline">Copy code</Button>
-      <Button className="outline">Docs</Button>
-     </div>
-    </div>
-    </div>
-  </SheetContent>
-</Sheet>
+        <Sheet>
+          <SheetTrigger>Open</SheetTrigger>
+          <SheetContent side="bottom" className="pt-12 pb-20 bg-white/70 backdrop-blur-md">
+            <div className="max-w-3xl w-full px-4 lg:px-0 mx-auto flex flex-col gap-8">
+              <div className="flex items-center justify-between">
+                <SheetTitle className="text-2xl font-medium">Button</SheetTitle>
+
+                <Link
+                  to={"/"}
+                  className="text-blue-500 hover:underline"
+                >
+                  View Docs
+                </Link>
+              </div>
+
+              <Bash code={`npm install wiii`} />
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
 };
 export default Shell;
 
-// const More=()=>{
-//   return(
+const Bash = ({ code }: { code: string }) => {
+  const copyCode = () => {
+    console.log("copied");
+  };
+  return (
+    <div className="flex items-center justify-between rounded-xl px-4 py-3 bg-black/3 gap-2">
+      <p className="font-code text-neutral-600 font-normal">{code}</p>
 
-//   )
-// }
-
-
-
-
-const Bash=({code}:{code: string})=>{
-  const copyCode=()=>{
-    console.log("copied")
-  }
-return(
-    <CodeBlock code={code} language={`bash`} theme={themes.github}>
-   <div className="rounded-xl px-6 py-4 bg-neutral-100">
-        <div className="flex items-center justify-between">
-        <CodeBlock.Code className="">
-          <div className="table-row">
-            <CodeBlock.LineContent className="table-cell">
-              <CodeBlock.Token />
-            </CodeBlock.LineContent>
-          </div>
-        </CodeBlock.Code>
-
-       <Button size="icon-lg" variant={`outline`} className={`bg-white hover:bg-white`}>
-          <Copy />
-          {/* {state.value ? 'Copied!' : 'Copy code'} */}
+      <Button
+        size="icon-lg"
+        variant={`outline`}
+        className={`bg-white/60 hover:bg-white/60`}
+      >
+        <Copy />
+        {/* {state.value ? 'Copied!' : 'Copy code'} */}
       </Button>
-        </div>
-  </div>
-    </CodeBlock>
-)
-}
+    </div>
+  );
+};
