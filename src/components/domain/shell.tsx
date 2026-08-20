41c8469ce74899197ffc964b2ef26daa7f0f70f7
sopo <sofiko.imnaishvili@gmail.com>
2026-08-20 20:31:44 +0400

    add bash

--- src/components/domain/shell.tsx
+++ src/components/domain/shell.tsx
@@ -8,12 +8,18 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import {themes} from "prism-react-renderer"
+
+import { CodeBlock } from 'react-code-block';
+
+
 import type { PropsWithChildren } from "react";
 
 import { Copy, Plus } from "lucide-react";
 import { Button } from "../ui/button";
 
 const Shell: React.FC<PropsWithChildren> = ({ children }) => {
+
   return (
     <div className="relative h-100 bg-card rounded-3xl flex items-center justify-center flex-col">
       <div>
@@ -45,7 +51,9 @@ const Shell: React.FC<PropsWithChildren> = ({ children }) => {
        <SheetTitle className="text-xl font-medium">Button</SheetTitle>
       <div>
         <h2 className="text-lg">Install</h2>
-        <p>npm i wi</p>
+        <div className="">
+            <Bash code={`npm install wiii`} />
+    </div>
       </div>
     <div className=" items-center gap-4 bg-red-200 flex">
      
@@ -73,3 +81,32 @@ export default Shell;
 
 //   )
 // }
+
+
+
+
+const Bash=({code}:{code: string})=>{
+  const copyCode=()=>{
+    console.log("copied")
+  }
+return(
+    <CodeBlock code={code} language={`bash`} theme={themes.github}>
+   <div className="rounded-xl px-6 py-4 bg-neutral-100">
+        <div className="flex items-center justify-between">
+        <CodeBlock.Code className="">
+          <div className="table-row">
+            <CodeBlock.LineContent className="table-cell">
+              <CodeBlock.Token />
+            </CodeBlock.LineContent>
+          </div>
+        </CodeBlock.Code>
+
+       <Button size="icon-lg" variant={`outline`} className={`bg-white hover:bg-white`}>
+          <Copy />
+          {/* {state.value ? 'Copied!' : 'Copy code'} */}
+      </Button>
+        </div>
+  </div>
+    </CodeBlock>
+)
+}
