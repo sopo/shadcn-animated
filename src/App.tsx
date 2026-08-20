29aab377dafc1610247920aba924e26531c93d61
sopo <sofiko.imnaishvili@gmail.com>
2026-08-20 14:15:26 +0400

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,74 +2,62 @@ import "./App.css";
 
 import { Button } from "./components/ui/button";
 
-import {
-  AnimatedTabs,
-  AnimatedTabsContent,
-  AnimatedTabsList,
-  AnimatedTabsTrigger,
-} from "./components/animated-tabs";
 import Hero from "./components/domain/hero";
 import Shell from "./components/domain/shell";
-import { CornerDownLeft } from "lucide-react";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 
 function App() {
   return (
     <div className="px-4">
-    <div className="max-w-4xl mx-auto flex flex-col gap-20">
-      <Hero />
-      <section className=" grid grid-cols-1 md:grid-cols-2 gap-2">
-        <Shell>
-          <Button className="bg-white text-black hover:bg-white rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
-            Click me
-          </Button>
-        </Shell>
+      <div className="max-w-4xl mx-auto flex flex-col gap-20">
+        <Hero />
+        <section className=" grid grid-cols-1 md:grid-cols-2 gap-2">
+          <Shell>
+            <Tabs defaultValue="account" className="w-[400px] ">
+              <TabsList className="">
+                <TabsTrigger value="account" className="" >Account</TabsTrigger>
+                <TabsTrigger value="password"  className="">Password</TabsTrigger>
+                <TabsTrigger value="asd"  className="">
+                tab 3
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="account">
+                Make changes to your account here.
+              </TabsContent>
+              <TabsContent value="password">
+                Change your password here.
+              </TabsContent>
+              <TabsContent value="asd">
+                Change your passwosdaasdrd here.
+              </TabsContent>
+            </Tabs>
+          </Shell>
 
-              <Shell>
-          <Button variant="default" className="rounded-full h-12 px-6">
-        Click me
-          </Button>
-        </Shell>
+          <Shell>
+            <Button className="bg-white text-black hover:bg-white rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
+              Click me
+            </Button>
+          </Shell>
 
+          <Shell>
+            <Button variant="default" className="rounded-full h-12 px-6">
+              Click me
+            </Button>
+          </Shell>
 
-
-        <Shell>
-          <div className="flex gap-4 items-center flex-col">
-            <AnimatedTabs
-              defaultValue="account"
-              className="flex flex-col items-center"
-            >
-              <AnimatedTabsList>
-                <AnimatedTabsTrigger value="account">
-                  Account
-                </AnimatedTabsTrigger>
-
-                <AnimatedTabsTrigger value="password">
-                  Password
-                </AnimatedTabsTrigger>
-              </AnimatedTabsList>
-
-              <AnimatedTabsContent value="account" className="text-center">
-               <p className="text-secondary-foreground">Make changes to your account here.</p> 
-              </AnimatedTabsContent>
-
-              <AnimatedTabsContent value="password">
-               <p className="text-secondary-foreground">Change your password here.</p> 
-              </AnimatedTabsContent>
-            </AnimatedTabs>
-          </div>
-        </Shell>
-         <Shell>
-          <Button className="bg-white text-black rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
-            Click me
-          </Button>
-        </Shell>
-         <Shell>
-          <Button className="bg-white text-black rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
-            Click me
-          </Button>
-        </Shell>
-      </section>
-    </div>
+          <Shell>
+            <Button className="bg-white text-black rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
+              Click me
+            </Button>
+          </Shell>
+          <Shell>
+            <Button className="bg-white text-black rounded-full py-6 px-8 shadow-[2px_4px_12px_0_rgba(0,0,20,0.08)]">
+              Click me
+            </Button>
+          </Shell>
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,8 +1,9 @@
-import { Tabs as TabsPrimitive } from "@base-ui/react/tabs"
-import { cva, type VariantProps } from "class-variance-authority"
+import { Tabs as TabsPrimitive } from "@base-ui/react/tabs";
+import { cva, type VariantProps } from "class-variance-authority";
+import { LayoutGroup, motion } from "motion/react";
 
-import { cn } from "@/lib/utils"
-
+import { cn } from "@/lib/utils";
+import { useId } from "react";
 function Tabs({
   className,
   orientation = "horizontal",
@@ -13,59 +14,90 @@ function Tabs({
       data-slot="tabs"
       data-orientation={orientation}
       className={cn(
-        "group/tabs flex gap-2 data-horizontal:flex-col",
-        className
+        "group/tabs flex gap-2 data-horizontal:flex-col h-10",
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
 const tabsListVariants = cva(
-  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg p-0.75 text-muted-foreground group-data-horizontal/tabs:h-8 group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col data-[variant=line]:rounded-none ",
+  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg py-5 px-0.5 text-muted-foreground group-data-horizontal/tabs:h-8 group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col data-[variant=line]:rounded-none ",
   {
     variants: {
       variant: {
-        default: "bg-muted",
+        default: "bg-muted ",
         line: "gap-1 bg-transparent",
       },
     },
     defaultVariants: {
       variant: "default",
     },
-  }
-)
+  },
+);
 
 function TabsList({
   className,
   variant = "default",
   ...props
 }: TabsPrimitive.List.Props & VariantProps<typeof tabsListVariants>) {
+  const id = useId();
   return (
-    <TabsPrimitive.List
-      data-slot="tabs-list"
-      data-variant={variant}
-      className={cn(tabsListVariants({ variant }), className)}
-      {...props}
-    />
-  )
+    <LayoutGroup id={id}>
+      <TabsPrimitive.List
+        data-slot="tabs-list"
+        data-variant={variant}
+        className={cn("flex gap-2", tabsListVariants({ variant }), className)}
+        {...props}
+      />
+    </LayoutGroup>
+  );
 }
 
 function TabsTrigger({ className, ...props }: TabsPrimitive.Tab.Props) {
   return (
+    // <TabsPrimitive.Tab
+    //   data-slot="tabs-trigger"
+    //   className={cn(
+    //     "cursor-pointer relative inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-1.5 py-0.5 text-sm font-medium whitespace-nowrap text-foreground/60  group-data-vertical/tabs:w-full group-data-vertical/tabs:justify-start hover:text-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50 has-data-[icon=inline-end]:pr-1 has-data-[icon=inline-start]:pl-1 aria-disabled:pointer-events-none aria-disabled:opacity-50 dark:text-muted-foreground dark:hover:text-foreground group-data-[variant=default]/tabs-list:data-active:shadow-sm group-data-[variant=line]/tabs-list:data-active:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    //     "group-data-[variant=line]/tabs-list:bg-transparent group-data-[variant=line]/tabs-list:data-active:bg-transparent dark:group-data-[variant=line]/tabs-list:data-active:border-transparent dark:group-data-[variant=line]/tabs-list:data-active:bg-transparent",
+    //     "data-active:bg-background data-active:text-foreground dark:data-active:border-input dark:data-active:bg-input/30 dark:data-active:text-foreground",
+    //     "after:absolute after:bg-foreground after:opacity-0 after:transition-opacity group-data-horizontal/tabs:after:inset-x-0 group-data-horizontal/tabs:after:-bottom-1.25 group-data-horizontal/tabs:after:h-0.5 group-data-vertical/tabs:after:inset-y-0 group-data-vertical/tabs:after:-right-1 group-data-vertical/tabs:after:w-0.5 group-data-[variant=line]/tabs-list:data-active:after:opacity-100",
+
+    //     className
+    //   )}
+
+    //   {...props}
+    // />
     <TabsPrimitive.Tab
       data-slot="tabs-trigger"
-      className={cn(
-        "cursor-pointer relative inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-1.5 py-0.5 text-sm font-medium whitespace-nowrap text-foreground/60  group-data-vertical/tabs:w-full group-data-vertical/tabs:justify-start hover:text-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50 has-data-[icon=inline-end]:pr-1 has-data-[icon=inline-start]:pl-1 aria-disabled:pointer-events-none aria-disabled:opacity-50 dark:text-muted-foreground dark:hover:text-foreground group-data-[variant=default]/tabs-list:data-active:shadow-sm group-data-[variant=line]/tabs-list:data-active:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        "group-data-[variant=line]/tabs-list:bg-transparent group-data-[variant=line]/tabs-list:data-active:bg-transparent dark:group-data-[variant=line]/tabs-list:data-active:border-transparent dark:group-data-[variant=line]/tabs-list:data-active:bg-transparent",
-        "data-active:bg-background data-active:text-foreground dark:data-active:border-input dark:data-active:bg-input/30 dark:data-active:text-foreground",
-        "after:absolute after:bg-foreground after:opacity-0 after:transition-opacity group-data-horizontal/tabs:after:inset-x-0 group-data-horizontal/tabs:after:-bottom-1.25 group-data-horizontal/tabs:after:h-0.5 group-data-vertical/tabs:after:inset-y-0 group-data-vertical/tabs:after:-right-1 group-data-vertical/tabs:after:w-0.5 group-data-[variant=line]/tabs-list:data-active:after:opacity-100",
-       
-        className
-      )}
+      render={(tabProps, state) => {
+        return (
+          <button
+            {...tabProps}
+            className={cn(
+              "relative inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-lg border border-transparent px-4 py-4 text-sm font-medium whitespace-nowrap text-foreground/60  group-data-vertical/tabs:w-full group-data-vertical/tabs:justify-start hover:text-foreground  focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50 has-data-[icon=inline-end]:pr-1 has-data-[icon=inline-start]:pl-1 aria-disabled:pointer-events-none aria-disabled:opacity-50 dark:text-muted-foreground dark:hover:text-foreground group-data-[variant=default]/tabs-list:data-active:shadow-none group-data-[variant=line]/tabs-list:data-active:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+              "group-data-[variant=line]/tabs-list:bg-transparent group-data-[variant=line]/tabs-list:data-active:bg-transparent dark:group-data-[variant=line]/tabs-list:data-active:border-transparent dark:group-data-[variant=line]/tabs-list:data-active:bg-transparent",
+              className,
+            )}
+          >
+            {state.active && (
+              <motion.div
+                layoutId={"active-tab"}
+                className=" absolute inset-0 bg-background rounded-md shadow-sm "
+              />
+            )}
+
+            <div className="relative z-10 cursor-pointer ">
+              {props.children}
+            </div>
+          </button>
+        );
+      }}
       {...props}
     />
-  )
+  );
 }
 
 function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
@@ -75,7 +107,7 @@ function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
       className={cn("flex-1 text-sm outline-none", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent, tabsListVariants }
+export { Tabs, TabsList, TabsTrigger, TabsContent, tabsListVariants };
