8986b510c38ebd74f632e7f2970d18331eb8e06b
sopo <sofiko.imnaishvili@gmail.com>
2026-08-21 10:00:26 +0400

    add button

--- src/domain/components/shell.tsx
+++ src/domain/components/shell.tsx
@@ -4,29 +4,31 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import type {  ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Plus } from "lucide-react";
 import { Link } from "react-router";
 import Bash from "./bash";
 
-interface ShellProps{
-
-
-    link: string,
-    title: string,
-    bash: string,
-
+interface ShellProps {
+  link: string;
+  title: string;
+  bash: string;
 }
 
-const Shell = ({ children,props }: {children:ReactNode, props: ShellProps}) => {
+const Shell = ({
+  children,
+  props,
+}: {
+  children: ReactNode;
+  props: ShellProps;
+}) => {
   return (
     <div className="relative h-100 bg-card rounded-3xl flex items-center justify-center flex-col">
       <div>{children}</div>
       <div className="flex items-center gap-2 w-full justify-between absolute bottom-0 p-4">
         <Sheet>
           <SheetTrigger>
-            <div className="rounded-full text-secondary-foreground">
-              Install
+            <div className="rounded-full text-secondary-foreground p-3 hover:bg-black/5 cursor-pointer scale-default">
               <Plus strokeWidth={2.5} />
             </div>
           </SheetTrigger>
@@ -36,7 +38,9 @@ const Shell = ({ children,props }: {children:ReactNode, props: ShellProps}) => {
           >
             <div className="max-w-3xl w-full px-4 lg:px-0 mx-auto flex flex-col gap-8">
               <div className="flex items-center justify-between">
-                <SheetTitle className="text-2xl font-medium">{props.title}</SheetTitle>
+                <SheetTitle className="text-2xl font-medium">
+                  {props.title}
+                </SheetTitle>
                 <Link to={props.link} className="text-blue-500 hover:underline">
                   View Docs
                 </Link>
