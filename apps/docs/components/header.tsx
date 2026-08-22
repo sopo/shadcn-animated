94edcfad405411ed06253a9d3c16fad858d4d938
Sopiko Imnaishvili <sofiko.imnaishvili@gmail.com>
2026-08-22 13:41:13 +0400

    add change icon

--- apps/docs/components/header.tsx
+++ apps/docs/components/header.tsx
@@ -4,6 +4,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { LINKS } from "@/lib/links";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "shadcn-animated";
+import { Equal, X } from "lucide-react";
+import { sections } from "./sidenav";
+import {  useState } from "react";
 
 const navItems = [
   { href: "/", label: "Home" },
@@ -13,11 +21,58 @@ const navItems = [
 
 const Header = () => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full bg-background/20 backdrop-blur-md">
-      <section className="max-w-4xl mx-auto flex p-4 justify-between">
-        <nav className="flex gap-4  items-center">
+    <header className={`fixed top-0 left-0 z-100 w-full backdrop-blur-md ${open ? "bg-background" : "bg-background/20"}`}>
+      <section className="max-w-4xl mx-auto flex p-4 justify-between items-center">
+        <div className="sm:hidden">
+        <Sheet open={open} onOpenChange={setOpen} >
+          <SheetTrigger className={`text-sm flex items-center gap-1`}>
+            {open? <X /> :<Equal />}
+            Menu
+          </SheetTrigger>
+          <SheetContent
+            side="top"
+            className="inset-y-0 bg-background"
+            showCloseButton={false}
+          >
+            <nav className="flex flex-col gap-8 overflow-x-auto px-5 pt-17">
+              {sections.map((section) => (
+                <div
+                  key={section.title}
+                  className="flex flex-col gap-3 min-w-fit"
+                >
+                  <h2 className="text-xs uppercase text-secondary-foreground">
+                    {section.title}
+                  </h2>
+                  {section.links.map((link) => {
+                    const isActive = pathname === link.href;
+
+                    return (
+                      <Link
+                        key={link.href}
+                        href={link.href}
+                        aria-current={isActive ? "page" : undefined}
+                        className={`text-sm transition-colors ${
+                          isActive
+                            ? "text-foreground"
+                            : "text-secondary-foreground hover:text-foreground"
+                        }`}
+                        onClick={() => setOpen(false)}
+                      >
+                        {link.label}
+                      </Link>
+                    );
+                  })}
+                </div>
+              ))}
+            </nav>
+          </SheetContent>
+        </Sheet>
+        </div>
+
+        <nav className="hidden sm:flex gap-4 items-center">
           {navItems.map((item) => {
             const isActive = pathname === item.href;
 
