12e5947c1b970db03721ed11b41eb20ac4f3f298
sopo <sofiko.imnaishvili@gmail.com>
2026-08-23 13:11:18 +0400

    add alpha badge (#20)

--- apps/docs/components/hero.tsx
+++ apps/docs/components/hero.tsx
@@ -4,7 +4,8 @@ import { LINKS } from "@/lib/links";
 const Hero = () => {
   return (
     <section className="mt-40 flex flex-col gap-4 items-center">
-      <div className="max-w-2xl mx-auto text-center flex flex-col gap-2">
+      <div className="max-w-2xl mx-auto text-center flex flex-col gap-2 items-center">
+        <Alpha />
         <h1 className="text-4xl sm:text-6xl font-medium">shadcn animated</h1>
         <h2 className="text-lg sm:text-2xl font-medium leading-[140%] text-secondary-foreground">
           Expanding collection of{" "}
@@ -26,3 +27,13 @@ const Hero = () => {
   );
 };
 export default Hero;
+
+const Alpha=()=>{
+  return(
+  
+    <div className="flex px-2 py-1 rounded-full bg-linear-to-b from-white to-primary/5 shadow-xs border border-border w-fit ">
+      <p className="text-[10px] font-bold text-secondary-foreground ">ALPHA</p>
+      </div>
+     
+  )
+}
