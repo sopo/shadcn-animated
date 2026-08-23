e27e82569e9c1227f284e6d16805886cc0f7f54f
Sopiko Imnaishvili <sofiko.imnaishvili@gmail.com>
2026-08-23 08:17:01 +0400

    update into

--- apps/docs/app/(docs)/introduction/page.tsx
+++ apps/docs/app/(docs)/introduction/page.tsx
@@ -12,12 +12,11 @@ const IntroductionPage = () => {
     <div className=" flex flex-col gap-4">
       <h1 className="text-3xl font-medium">Introduction</h1>
       <div className="">
-        <p className="font-medium text-secondary-foreground text-lg leading-[150%]">
-          <span className="font-medium"><span className="text-foreground">shadcn animated</span> is a growing collection of <span className="text-foreground">animated shadcn/ui components</span>,
-          crafted by hand.</span> Every interaction and motion is designed to feel
-          natural, refined, and effortless—bringing more life to interfaces.
+        <p className="text-secondary-foreground leading-[150%]">
+          <span><span className="text-foreground">shadcn animated</span> is a growing collection of <span className="text-foreground">animated shadcn/ui components</span>,
+          crafted by hand.</span>
           <br></br><span className="text-foreground">Copy and paste</span> the components directly into your project, or install
-          them with the CLI. <br></br><br></br> <span className="text-foreground">New components are added every week.</span>
+          them with the CLI. <br></br><br></br> <span className="text-foreground">New components are added weekly.</span>
         </p>
       </div>
       <NextSection title="Installation" link={LINKS.INSTALLATION} />
