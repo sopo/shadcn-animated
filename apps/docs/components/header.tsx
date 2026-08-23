e22df18be8ec6b624e3d0aff6bccce2459403b12
Sopiko Imnaishvili <sofiko.imnaishvili@gmail.com>
2026-08-23 09:29:43 +0400

    fix header

--- apps/docs/components/header.tsx
+++ apps/docs/components/header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 z-100 w-full backdrop-blur-md ${open ? "bg-background" : "bg-background/20"}`}
+      className={`fixed top-0 left-0 z-100 sm:z-20 w-full backdrop-blur-md ${open ? "bg-background" : "bg-background/20"}`}
     >
       <section className="max-w-4xl mx-auto flex p-4 justify-between items-center">
         <div className="sm:hidden">
