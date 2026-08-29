35c0c165d8953a9b170851964b0a04ee5f195f55
sopo <sofiko.imnaishvili@gmail.com>
2026-08-29 11:20:11 +0400

    add style

--- apps/docs/components/accordion/accordion-preview.tsx
+++ apps/docs/components/accordion/accordion-preview.tsx
@@ -4,22 +4,22 @@ const AccordionPreview = () => {
   return (
   <Accordion defaultValue={["shipping"]} className="w-65 max-w-lg sm:w-lg rounded-2xl bg-white p-4 h-auto">
       <AccordionItem value="shipping"  className={``}>
-        <AccordionTrigger className={`hover:no-underline `}>What are your shipping options?</AccordionTrigger>
-        <AccordionContent>
+        <AccordionTrigger  className={`hover:no-underline text-lg font-medium **:data-[slot=accordion-trigger-icon]:size-5`}>What are your shipping options?</AccordionTrigger>
+        <AccordionContent className={`text-secondary-foreground`}>
           We offer standard (5-7 days), express (2-3 days), and overnight
           shipping. Free shipping on international orders.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="returns" >
-        <AccordionTrigger className={`hover:no-underline `}>What is your return policy?</AccordionTrigger>
-        <AccordionContent>
+        <AccordionTrigger className={`hover:no-underline text-lg font-medium **:data-[slot=accordion-trigger-icon]:size-5`}>What is your return policy?</AccordionTrigger>
+        <AccordionContent className={`text-secondary-foreground`}>
           Returns accepted within 30 days. Items must be unused and in original
           packaging. Refunds processed within 5-7 business days.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="support">
-        <AccordionTrigger className={`hover:no-underline `}>How can I contact customer support?</AccordionTrigger>
-        <AccordionContent>
+        <AccordionTrigger className={`hover:no-underline text-lg font-medium **:data-[slot=accordion-trigger-icon]:size-5`}>How can I contact customer support?</AccordionTrigger>
+        <AccordionContent className={`text-secondary-foreground`}>
           Reach us via email, live chat, or phone. We respond within 24 hours
           during business days.
         </AccordionContent>
