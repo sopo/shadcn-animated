ab865d69785453f38dc865f2ef04fa7ec7862d8f
Sopiko Imnaishvili <sofiko.imnaishvili@gmail.com>
2026-09-26 19:44:05 +0400

    update preview

--- apps/docs/app/(docs)/icon-toggle-preview.tsx
+++ apps/docs/app/(docs)/icon-toggle-preview.tsx
@@ -1,17 +1,14 @@
 import { Check, Copy } from "lucide-react";
 import { IconToggle } from "shadcn-animated";
 
-
 const IconTogglePreview = () => {
- 
-
   return (
-  <IconToggle
-
-  start={<Copy />}
-  end={<Check />}
-/>
-
+    <IconToggle
+    variant="ghost"
+      className={`p-8 rounded-full hover:bg-backgound`}
+      start={<Copy className="size-6" />}
+      end={<Check className="size-6" />}
+    />
   );
 };
-export default IconTogglePreview
+export default IconTogglePreview;
